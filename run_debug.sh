25d79d089d63752f2277f434567d14e78af9b824
linull <linull@shu.edu.cn>
2025-06-29 12:00:17 +0800

    begining

--- run_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd Debug
-./sorts
-echo "Program executed in Debug mode. Results saved to Debug/results.txt"
--- run_release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd Release
-./sorts
-echo "Program executed in Release mode. Results saved to Release/results.txt"
